ec5217364b9b4bd9680724e08561803e391df6ed
Petr Andreev <dmitrijandreev@MacBook-Pro-Dmitrij.local>
2024-09-18 18:51:43 +0300

    template

---
+++ bot/config.py
@@ -0,0 +1,58 @@
+from dataclasses import dataclass
+
+from environs import Env
+
+
+@dataclass
+class TgBot:
+    token: str
+
+
+@dataclass
+class Postgres:
+    postgres_host: str
+    postgres_db: str
+    postgres_password: str
+    postgres_port: int
+    postgres_user: str
+
+    def get_connection_url(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}"
+            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+        )
+
+@dataclass
+class Redis:
+    redis_host: str
+    redis_port: int
+    redis_db: int
+    redis_data: str
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    postgres_db: Postgres
+    redis_db: Redis
+
+
+def load_config(path: str | None = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(
+        tg_bot=TgBot(token=env("BOT_TOKEN")),
+        postgres_db=Postgres(
+            postgres_host=env("POSTGRES_HOST"),
+            postgres_db=env("POSTGRES_DB"),
+            postgres_password=env("POSTGRES_PASSWORD"),
+            postgres_port=env.int("POSTGRES_PORT"),
+            postgres_user=env("POSTGRES_USER")
+        ),
+        redis_db=Redis(
+            redis_host=env("REDIS_HOST"),
+            redis_port=env("REDIS_PORT"),
+            redis_db=env("REDIS_DB"),
+            redis_data=env("REDIS_DATA")
+        )
+    )
---
+++ bot/scheduler.py
@@ -0,0 +1,22 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db.crud import crud_note, crud_user
+from db.db import User
+
+MINUTES = 10
+
+
+# async def check_reminders(session_pool, client: TelegramClient):
+#     async with session_pool() as session:
+#         reminders = await crud_note.get_notes_for_remind(
+#             session=session, minutes=MINUTES
+#         )
+#         for note in reminders:
+#             await notify_user(note.user_id, note.text, session, client)
+
+
+# async def notify_user(
+#     user_id: int, text: str, session: AsyncSession, client: TelegramClient
+# ):
+#     user: User = await crud_user.get(user_id, session)
+#     await client.send_message(user.telegram_id, f"Напоминание: {text}")
